f0e303e6554a366d24f7edd1c6ee8bd491707db2
James Black <james@james-black.com>
2025-11-28 03:36:06 -0600

    chore: A better CMAKE process for generating shaders.
    This works more consistently when the .frag/.vert files change.

---
+++ Source/Client/cmake/generate_shader.cmake
@@ -0,0 +1,11 @@
+﻿file(READ ${FRAG} FRAGMENT_SHADER_CONTENT)
+file(READ ${VERT} VERTEX_SHADER_CONTENT)
+
+set(FRAGMENT_SHADER_NAME "${SHADER_NAME}_fragment")
+set(VERTEX_SHADER_NAME "${SHADER_NAME}_vertex")
+
+configure_file(
+        ${TEMPLATE}
+        ${OUT}
+        @ONLY
+)
