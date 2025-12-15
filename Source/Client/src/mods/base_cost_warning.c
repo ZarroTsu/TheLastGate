d1fb28492d9ae22d74c78998c61403f6667bf67e
James Black <james@james-black.com>
2025-12-15 01:13:10 -0600

    chore: fix base_cost_warning.c not supporting hara uber scaling.

--- Source/Client/src/mods/base_cost_warning.c
+++ Source/Client/src/mods/base_cost_warning.c
@@ -1,5 +1,7 @@
 ﻿#include "base_cost_warning.h"
 
+#include <stdbool.h>
+
 #include "common.h"
 #include "engine.h"
 #include "inter.h"
@@ -11,6 +13,43 @@ typedef struct {
 
 #define ATTRIBUTE_TARGETS_FROM_INITIAL_VALUES(iv) \
     { (iv)[0], (iv)[1], (iv)[2], (iv)[3], (iv)[4] }
+
+static bool is_weapon_skill(int skill_id) {
+    switch (skill_id) {
+        case SK_HAND:
+        case SK_AXE:
+        case SK_DAGGER:
+        case SK_STAFF:
+        case SK_SWORD:
+        case SK_TWOHAND:
+            return true;
+        default:
+            return false;
+    }
+}
+
+static bool is_spell(int skill_id) {
+    switch (skill_id) {
+        case SK_BLAST:
+        case SK_BLESS:
+        case SK_CURSE:
+        case SK_DISPEL:
+        case SK_DISPEL2:
+        case SK_ENHANCE:
+        case SK_GHOST:
+        case SK_HASTE:
+        case SK_HEAL:
+        case SK_MSHIELD:
+        case SK_POISON:
+        case SK_PROTECT:
+        case SK_PULSE:
+        case SK_SHADOW:
+        case SK_SLOW:
+            return true;
+        default:
+            return false;
+    }
+}
 
 static AttributeCostResult cost_to_raise_attributes_by_5(const int attributes[3]) {
     int initial_values[5] = {pl.attrib[0][0], pl.attrib[1][0], pl.attrib[2][0], pl.attrib[3][0], pl.attrib[4][0]};
@@ -58,7 +97,7 @@ static AttributeCostResult cost_to_raise_attributes_by_5_uber(const int attribut
     while (attempts < 10) {
         int lowest_cost_index = -1;
         int lowest_cost = HIGH_VAL;
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 4; i++) {
             int a = attributes[i];
             int c = attrib_needed(a, initial_values[a]);
             if (c < lowest_cost) {
@@ -92,8 +131,10 @@ char get_raise_icon(int skill_id, int skill_tab_position) {
     int cost_to_raise_skill = skill_needed(skill_id, pl.skill[skill_id][0] + stat_raised[skill_tab_position + 8]);
 
     AttributeCostResult result;
-    if (skill_tab_position == 5 && T_BRAV_SK(9)) {
+    if (is_weapon_skill(skilltab[skill_tab_position].nr) && T_BRAV_SK(9)) {
         result = cost_to_raise_attributes_by_5_uber((int[4]){AT_BRV, AT_BRV, AT_AGL, AT_STR});
+    } else if (is_spell(skilltab[skill_tab_position].nr) && T_ARHR_SK(9)) {
+        result = cost_to_raise_attributes_by_5_uber((int[4]){AT_INT, AT_INT, AT_WIL, AT_BRV});
     } else {
         result = cost_to_raise_attributes_by_5(skilltab[skill_tab_position].attrib);
     }
