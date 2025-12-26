9b981e0fa5a8d6d2f3341366c05192e6eb17b2bd
James Black <james@james-black.com>
2025-12-26 14:31:14 -0600

    refactor: Add Vec2 struct, mirror ImGui ImVec2 structure

--- Source/Client/src/util/math_util.h
+++ Source/Client/src/util/math_util.h
@@ -1,4 +1,11 @@
 #pragma once
 
+typedef struct {
+    float x;
+    float y;
+} Vec2;
+
+static Vec2 vec2(float x, float y) { return (Vec2){x, y}; }
+
 static int min(int a, int b) { return a < b ? a : b; }
 static int max(int a, int b) { return a > b ? a : b; }
