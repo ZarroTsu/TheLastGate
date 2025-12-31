9e2a767403f696e7228da47551736393447653d0
ZarroTsu <zarrotsu@hotmail.com>
2025-12-31 10:51:44 -0500

    Right-click blurb shortener 9000

--- Source/Client/inter.c
+++ Source/Client/inter.c
@@ -1311,7 +1311,7 @@ int mouse_statbox2(int x,int y,int state)
 				else
 				{
 					last_skill = n+skill_pos;
-					if (!firstclick)
+					if (!firstclick && (IS_TEMPLAR || IS_MERCENARY || IS_HARAKIM))
 					{
 						xlog(6,"%s selected for shortcut. Right-click on one of the shortcut keys in the bottom right to set a shortcut.",tmp);
 						firstclick=1;
