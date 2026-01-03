94ed5c9d13282c08e03074faa88f65f7d142298b
James Black <james@james-black.com>
2026-01-03 02:00:00 -0600

    chore(cmake): Move the include directories to the root cmake

--- Source/Client/cmake/link_libraries.cmake
+++
@@ -1,8 +0,0 @@
-# Link libraries - vcpkg-based dependency linking
-
-# Add include directories for project structure
-target_include_directories(TheLastGate PRIVATE
-        ${CMAKE_BINARY_DIR}/_generated
-        ${CMAKE_CURRENT_SOURCE_DIR}/src/
-        ${CMAKE_CURRENT_SOURCE_DIR}/
-)
