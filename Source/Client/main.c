4f3394b33ff3050abb042ab6ce967a667f124bc9
James Black <james@james-black.com>
2026-01-05 02:15:57 -0600

    chore: bump version
    Release-As: 3.7.0

--- Source/Client/main.c
+++ Source/Client/main.c
@@ -26,7 +26,7 @@
 #include "ui/imgui/imgui_wrapper.h"
 #include "util/perf.h"
 
-const SdlClientVersion CLIENT_VERSION = {3,6};
+const SdlClientVersion CLIENT_VERSION = {3,7};
 
 // Screen data, can be shared with other files via extern
 int screen_width, screen_height, screen_tilexoff, screen_tileyoff, screen_viewsize, view_subedges;
