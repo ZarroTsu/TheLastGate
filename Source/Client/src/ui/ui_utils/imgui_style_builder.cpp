9502ab796c2f74126d6ea75ed8c809e813025b4f
James Black <james@james-black.com>
2026-01-04 19:37:46 -0600

    refactor: add new imgui style builder to make defining/cleaning styles easier

---
+++ Source/Client/src/ui/ui_utils/imgui_style_builder.cpp
@@ -0,0 +1,53 @@
+//
+// Created by james on 1/4/2026.
+//
+
+#include "imgui_style_builder.hpp"
+
+ImGuiStyleBuilder &ImGuiStyleBuilder::Var(ImGuiStyleVar idx, float val) {
+    ImGui::PushStyleVar(idx, val);
+    ++vars_pushed;
+    return *this;
+}
+
+ImGuiStyleBuilder &ImGuiStyleBuilder::Var(ImGuiStyleVar idx, const ImVec2 &vec) {
+    ImGui::PushStyleVar(idx, vec);
+    ++vars_pushed;
+    return *this;
+}
+
+ImGuiStyleBuilder &ImGuiStyleBuilder::Color(ImGuiCol idx, ImU32 val) {
+    ImGui::PushStyleVar(idx, val);
+    ++vars_pushed;
+    return *this;
+}
+
+ImGuiStyleBuilder &ImGuiStyleBuilder::Font(ImFont *font) {
+    ImGui::PushFont(font);
+    ++fonts_pushed;
+    return *this;
+}
+
+std::function<void()> ImGuiStyleBuilder::Build() {
+    int vars = vars_pushed;
+    int colors = colors_pushed;
+    int fonts = fonts_pushed;
+
+    vars_pushed = 0;
+    colors_pushed = 0;
+    fonts_pushed = 0;
+
+    return [vars, colors, fonts]() {
+        if (vars > 0) {
+            ImGui::PopStyleVar(vars);
+        }
+
+        if (colors > 0) {
+            ImGui::PopStyleColor(colors);
+        }
+
+        if (fonts > 0) {
+            ImGui::PopFont();
+        }
+    };
+}
---
+++ Source/Client/src/ui/ui_utils/imgui_style_builder.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <imgui.h>
+#include <functional>
+
+class ImGuiStyleBuilder {
+public:
+    ImGuiStyleBuilder() = default;
+
+    ImGuiStyleBuilder &Var(ImGuiStyleVar idx, float val);
+
+    ImGuiStyleBuilder &Var(ImGuiStyleVar idx, const ImVec2 &vec);
+
+    ImGuiStyleBuilder &Color(ImGuiCol idx, ImU32 val);
+
+    ImGuiStyleBuilder &Font(ImFont *font);
+
+    std::function<void()> Build();
+
+private:
+    int vars_pushed = 0;
+    int colors_pushed = 0;
+    int fonts_pushed = 0;
+};
