b13fcf203512615d16f59bb15f08219d556cbabc
James Black <james@james-black.com>
2026-02-05 12:02:46 -0600

    refactor: add an Integer Vector2
    no more int conversion warnings

--- Source/Client/src/util/math_util.h
+++ Source/Client/src/util/math_util.h
@@ -5,7 +5,13 @@ typedef struct {
     float y;
 } Vec2;
 
+typedef struct {
+    int x;
+    int y;
+} IVec2;
+
 static Vec2 vec2(float x, float y) { return (Vec2){x, y}; }
+static IVec2 ivec2(int x, int y) { return (IVec2){x, y}; }
 
 static int min(int a, int b) { return a < b ? a : b; }
 static int max(int a, int b) { return a > b ? a : b; }
